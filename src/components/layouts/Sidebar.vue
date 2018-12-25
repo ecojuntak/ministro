8e2f45373b9530ce96a35dfb81454978b57822ca
totapetro69 <if415031@students.del.ac.id>
2018-12-25 08:16:07 +0700

    Asset library, try to make header & Sidebar

---
+++ src/components/layouts/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="/"><img src="../../admin-assets/images/admin.jpg" alt="Logo"></a>
+                <a class="navbar-brand hidden" href="/"><img src="../../admin-assets/images/admin.jpg" alt="Logo"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <h3 class="menu-title">Merchant</h3>
+                    <li> <a href=""> <i class="menu-icon fa fa-user"></i>New Register Merchant </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-users"></i>List of Merchant </a> </li>
+
+                    <h3 class="menu-title">Order</h3>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> New Order </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> Unpaid Order </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> Paid Order </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> Invalid Order </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> On Process Order </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-archive"></i> Successed Order </a> </li>
+
+                    <h3 class="menu-title">View</h3>
+                    <li> <a href=""> <i class="menu-icon fa fa-star"></i> Carousel </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-bookmark"></i> Banner </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-stack-exchange"></i> Blogs </a> </li>
+
+                    <h3 class="menu-title">Role & Permissions</h3>
+
+                    <li> <a href=""> <i class="menu-icon fa fa-users"></i> Roles </a> </li>
+                    <li> <a href=""> <i class="menu-icon fa fa-user"></i> Permissions </a> </li>
+                </ul>
+            </div>
+        </nav>
+    </aside>
+</template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,27 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+
+require ('@/admin-assets/vendors/bootstrap/dist/css/bootstrap.min.css')
+require ('@/admin-assets/vendors/font-awesome/css/font-awesome.min.css')
+require('@/admin-assets/vendors/font-awesome/css/font-awesome.min.css')
+require('@/admin-assets/vendors/themify-icons/css/themify-icons.css')
+require('@/admin-assets/vendors/flag-icon-css/css/flag-icon.min.css')
+require('@/admin-assets/vendors/selectFX/css/cs-skin-elastic.css')
+require('@/admin-assets/vendors/jqvmap/dist/jqvmap.min.css')
+require('@/admin-assets/vendors/jqvmap/dist/jqvmap.min.css')
+require('@/admin-assets/assets/css/style.css')
+
+require ('@/admin-assets/vendors/jquery/dist/jquery.min.js')
+// require ('@/admin-assets/vendors/popper.js/dist/umd/popper.min.js')
+// require ('@/admin-assets/vendors/chart.js/dist/Chart.bundle.min.js')
+require ('@/admin-assets/assets/js/main.js')
+// require ('@/admin-assets/assets/js/dashboard.js')
+// require ('@/admin-assets/assets/js/widgets.js')
+require ('@/admin-assets/vendors/jqvmap/dist/jquery.vmap.min.js')
+require ('@/admin-assets/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')
+require ('@/admin-assets/vendors/jqvmap/dist/maps/jquery.vmap.world.js')
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -11,3 +32,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
